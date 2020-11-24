a90257289a98f0cec4f1738a3e1e8ecfe1a5bc4e
afriyudi <73121294+afriyudi@users.noreply.github.com>
2020-11-24 11:10:02 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Data Mahasiswa';
+    final appTitle = 'Data Mahasiswa ubd';
 
     return MaterialApp(
       title: appTitle,
